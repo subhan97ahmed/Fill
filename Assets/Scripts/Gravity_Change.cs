af0050fd29b4cd0da53e3382f460e090dc080efa
subhan <subhan97ahmed@gmail.com>
2021-10-01 02:16:57 +0500

    initial commit after 3 days of work

--- Assets/Scripts/Gravity_Change.cs
+++ Assets/Scripts/Gravity_Change.cs
@@ -8,6 +8,8 @@ public class Gravity_Change : MonoBehaviour
     // Start is called before the first frame update
     public string x;
     public string y;
+    public bool soundActive= false;
+    public AudioSource audioSource;
     void Start()
     {
         
@@ -22,17 +24,18 @@ public class Gravity_Change : MonoBehaviour
         float y_axis = Input.acceleration.y;
          if(Input.GetKey(KeyCode.W)){
             Physics2D.gravity =new Vector2(0,9.8f);
-
+            playWaterSound();
         }
-        else if(Input.GetKey(KeyCode.S)){
+         if(Input.GetKey(KeyCode.S)){
             Physics2D.gravity =new Vector2(0,-9.8f);
-            
+            playWaterSound();
         }
-        else if(Input.GetKey(KeyCode.D)){
+         if(Input.GetKey(KeyCode.D)){
             Physics2D.gravity =new Vector2(9.8f,0);
-            
+            playWaterSound();
         }
-        else if(Input.GetKey(KeyCode.A)){
+         if(Input.GetKey(KeyCode.A)){
+             playWaterSound();
             Physics2D.gravity =new Vector2(-9.8f,0);
             
         }
@@ -54,31 +57,40 @@ public class Gravity_Change : MonoBehaviour
             
     //     }
 
-    // testing 
-    
-    
+    // testing water movement on phone movement
+     if (!x.Equals("0") && !y.Equals("0")){
+         
          if(Input.deviceOrientation ==DeviceOrientation.LandscapeRight){
             Physics2D.gravity =new Vector2(9.8f,0);
+            playWaterSound();
         }else if(Input.deviceOrientation ==DeviceOrientation.LandscapeLeft){
             Physics2D.gravity =new Vector2(-9.8f,0);
-            
-        }
-     else if( (y_axis>-0.4 && x_axis <0.7f )|| (y_axis>-0.4 && x_axis>-0.7f) ){
+            playWaterSound();
+            }
+         else if( (y_axis>-0.4 && x_axis <0.7f )|| (y_axis>-0.4 && x_axis>-0.7f) ){
             Physics2D.gravity =new Vector2(0,9.8f);
-
+            playWaterSound();
         }
         else if(x_axis<0.7f && x_axis>-0.7f){
             Physics2D.gravity =new Vector2(0,-9.8f);
-            
+            playWaterSound();
         }
         else if(x_axis >0.7f){
             Physics2D.gravity =new Vector2(9.8f,0);
-            
+            playWaterSound();
         }
         else if(x_axis<-0.7f){
             Physics2D.gravity =new Vector2(-9.8f,0);
-            
+            playWaterSound();
         }
-       
+     }
+    }
+    // will play sound when the water moves
+    void playWaterSound(){
+        if(soundActive){
+        float rad = Random.Range(1.0f,3.0f);
+        audioSource.pitch=rad;
+        audioSource.Play();
+        }
     }
 }
---
+++ Assets/auto_gen/LevelGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] objects;
+    public float[] rotation;
+    void Start()
+    {
+
+        int ran = Random.Range(0,objects.Length);
+        int ran2 =Random.Range(0,rotation.Length);
+
+        Instantiate(objects[ran],transform.position,Quaternion.Euler(0.0f,0.0f,rotation[ran2]));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
